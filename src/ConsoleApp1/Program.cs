c47fac42fa7f528cd4daaf684c5a5f6d8c093ff7
Khilan Goel <khgoel@microsoft.com>
2021-06-01 14:38:38 +0530

    Build issues

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Kusto.Language;
+using Kusto.Language.Symbols;
+using Kusto.Language.Syntax;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // parse only
+            var query = "T | project a = a + b | where a > 10.0";
+            var code = KustoCode.Parse(query);
+        }
+    }
+}
